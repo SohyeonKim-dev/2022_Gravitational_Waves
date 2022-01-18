2a3366fcb9ba6ee8ec40ee662354073b4b300630
SoHyeon Kim <happythgus89@gmail.com>
2022-01-18 17:42:35 +0900

    Create import pycbc_3.py

---
+++ import pycbc_3.py
@@ -0,0 +1,35 @@
+import pycbc
+import matplotlib.pyplot as plt
+from pycbc import frame
+import pylab
+from pycbc.catalog import Merger
+from pycbc.filter import resample_to_delta_t, highpass
+from gwpy.timeseries import TimeSeries
+
+strain = frame.read_frame("BBH1-H1.gwf","H1:HWINJ_INJECTED")
+
+strain = highpass(strain, 30.0)
+strain = resample_to_delta_t(strain, 1.0/2048)
+conditioned = strain.crop(2, 2)
+
+from pycbc.psd import interpolate, inverse_spectrum_truncation
+psd = conditioned.psd(4)
+psd = interpolate(psd, conditioned.delta_f)
+psd = inverse_spectrum_truncation(psd, int(4 * conditioned.sample_rate),low_frequency_cutoff=20)
+
+from pycbc.waveform import get_td_waveform
+m = 36 
+hp, hc = get_td_waveform(approximant="SEOBNRv4_opt", mass1=m, mass2=m, delta_t=conditioned.delta_t, f_lower=20)
+pylab.figure()
+pylab.title('Before shifting')
+pylab.plot(hp.sample_times, hp)
+pylab.xlabel('Time (s)')
+pylab.ylabel('Strain')
+pylab.savefig('1st.png')
+template = hp.cyclic_time_shift(hp.start_time)
+pylab.figure()
+pylab.title('After shifting')
+pylab.plot(template.sample_times, template)
+pylab.xlabel('Time (s)')
+pylab.ylabel('Strain')
+pylab.savefig('2nd.png')
